5692d32df1921dd29d8820dca01bc0a1aa5cf5a1
Dias12 <diasoryntayev@gmail.com>
2022-10-19 15:34:50 +0600

    feat: add method doRequest

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -1,9 +1,12 @@
 package telegram
 
 import (
+	"io"
 	"net/http"
 	"net/url"
+	"path"
 	"strconv"
+	"tg-bot/lib/e"
 )
 
 type Client struct {
@@ -29,7 +32,32 @@ func (c *Client) Updates(offset int, limit int) ([]Update, error) {
 	q.Add("offset", strconv.Itoa(offset))
 	q.Add("limit", strconv.Itoa(limit))
 
-	// do request
+	// do request <- getUpdates
+}
+
+func (c *Client) doRequest(method string, query url.Values) (data []byte, err error) {
+	defer func() { err = e.WrapIfErr("can't do request", err) }()
+
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	resp.Body.Close()
+
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
 }
 
 func (c *Client) SendMessage() {
