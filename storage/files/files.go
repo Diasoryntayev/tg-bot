6f8e4552fb3e7a2df4b921045cae04720e1b4e15
Dias12 <diasoryntayev@gmail.com>
2022-10-21 17:26:26 +0600

    feat: add method Save a

--- storage/files/files.go
+++ storage/files/files.go
@@ -1,5 +1,12 @@
 package files
 
+import (
+	"os"
+	"path/filepath"
+	"tg-bot/lib/e"
+	"tg-bot/storage"
+)
+
 type Storage struct {
 	basePath string
 }
@@ -8,4 +15,14 @@ const defaultPerm = 0774
 
 func New(basePath string) Storage {
 	return Storage{basePath: basePath}
+}
+
+func (s Storage) Save(page *storage.Page) (err error) {
+	defer func() { err = e.WrapIfErr("can't save", err) }()
+
+	filePath := filepath.Join(s.basePath, page.UserName)
+
+	if err := os.MkdirAll(filePath, defaultPerm); err != nil {
+		return err
+	}
 }
