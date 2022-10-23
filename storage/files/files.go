9853a891deca6acbb069bb04c26bd5788ed1e5fc
Dias12 <diasoryntayev@gmail.com>
2022-10-23 17:31:14 +0600

    feat: add method IsExists

--- storage/files/files.go
+++ storage/files/files.go
@@ -89,6 +89,24 @@ func (s Storage) Remove(p *storage.Page) error {
 	return nil
 }
 
+func (s Storage) IsExist(p *storage.Page) (bool, error) {
+	fileName, err := fileName(p)
+	if err != nil {
+		return false, e.Wrap("can't check if file exists", err)
+	}
+
+	path := filepath.Join(s.basePath, p.UserName, fileName)
+
+	switch _, err = os.Stat(path); {
+	case errors.Is(err, os.ErrNotExist):
+		return false, nil
+	case err != nil:
+		msg := fmt.Sprintf("can't check if file %s exists", path)
+		return false, e.Wrap(msg, err)
+	}
+	return true, nil
+}
+
 func (s Storage) decodePage(filePath string) (*storage.Page, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
