a559d925207f276c86fbbf1e6517ccbe3a6e521a
Dias12 <diasoryntayev@gmail.com>
2022-10-23 17:35:22 +0600

    feat: func New

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,11 +1,19 @@
 package telegram
 
-import "tg-bot/clients/telegram"
+import (
+	"tg-bot/clients/telegram"
+	"tg-bot/storage"
+)
 
 type Processor struct {
-	tg     *telegram.Client
-	offset int
-	// storage
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
 }
 
-func New()
+func New(client *telegram.Client, storage storage.Storage) *Processor {
+	return &Processor{
+		tg:      client,
+		storage: storage,
+	}
+}
