1aa7baa381c9898aabf896041f4960a18456c857
Dias12 <diasoryntayev@gmail.com>
2022-10-25 21:15:25 +0600

    feat: update method Fetch

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -26,16 +26,23 @@ func New(client *telegram.Client, storage storage.Storage) *Processor {
 }
 
 func (p *Processor) Fetch(limit int) ([]events.Event, error) {
-	update, err := p.tg.Updates(p.offset, limit)
+	updates, err := p.tg.Updates(p.offset, limit)
 	if err != nil {
 		return nil, e.Wrap("can't get events", err)
 	}
 
-	res := make([]events.Event, 0, len(update))
+	if len(updates) == 0 {
+		return nil, nil
+	}
 
-	for _, u := range update {
+	res := make([]events.Event, 0, len(updates))
+
+	for _, u := range updates {
 		res = append(res, event(u))
 	}
+
+	p.offset = updates[len(updates)-1].ID + 1
+	return res, nil
 }
 
 func event(upd telegram.Update) events.Event {
