a27f5827f3f132ccf7e630d93aa5e2211d1cab89
Dias12 <diasoryntayev@gmail.com>
2022-10-30 17:22:04 +0600

    feat: add Start

--- consumer/event-consumer/event-consumer.go
+++ consumer/event-consumer/event-consumer.go
@@ -1,7 +1,9 @@
 package event_consumer
 
 import (
+	"log"
 	"tg-bot/events"
+	"time"
 )
 
 type Consumer struct {
@@ -15,5 +17,19 @@ func New(fetcher events.Fetcher, processor events.Processor, batchSize int) Cons
 		fetcher:   fetcher,
 		processor: processor,
 		batchSize: batchSize,
+	}
+}
+
+func (c Consumer) Start() error {
+	for {
+		gotEvents, err := c.fetcher.Fetch(c.batchSize)
+		if err != nil {
+			log.Printf("[ERR] consumer: %s", err.Error())
+			continue
+		}
+		if len(gotEvents) == 0 {
+			time.Sleep(1 * time.Second)
+			continue
+		}
 	}
 }
